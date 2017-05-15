2333d4d5dfa7b4b98a5f43de48d80ad1d57b760f
John McBride <john@johnmcbride.me>
2017-05-14 23:40:10 -0400

    Added prompt support for the netscaler CPX

--- generators/cpxsupport/index.js
+++ generators/cpxsupport/index.js
@@ -12,8 +12,47 @@ module.exports = yeoman.Base.extend({
       type    : 'confirm',
       name    : 'nsCPX',
       message : 'Would you like to add Netscaler CPX to ' + this.config.get('appName')
+    },
+    {
+      when    : function(response) {
+        return response.nsCPX;
+      },
+      type    : 'input',
+      name    : 'nsCPXHttpPort',
+      message : 'Please enter the local HTTP port to map to the http port in the docker container',
+      default : '32777'
+    },   
+    {
+      when    : function(response) {
+        return response.nsCPX;
+      },
+      type    : 'input',
+      name    : 'nsCPXHttpsPort',
+      message : 'Please enter the local HTTPS port to map to the https port in the docker container',
+      default : '32778'
+    }, 
+    {
+      when    : function(response) {
+        return response.nsCPX;
+      },
+      type    : 'input',
+      name    : 'nsCPXSSHPort',
+      message : 'Please enter the local SSH port to map to the SSH port in the docker container',
+      default : '32779'
+    },    
+    {
+      when    : function(response) {
+        return response.nsCPX;
+      },
+      type    : 'input',
+      name    : 'nsCPXSNMPPort',
+      message : 'Please enter the local SNMP port to map to the SSH port in the docker container',
+      default : '32780'
     }]).then(function (answers) {
       this.config.set('nsCPX',answers.nsCPX);
+      this.config.set('nsCPXHttpPort',answers.nsCPXHttpPort);
+      this.config.set('nsCPXSSHPort',answers.nsCPXSSHPort);
+      this.config.set('nsCPXSNMPPort',answers.nsCPXSNMPPort);
       this.config.save();
     }.bind(this));
   },
@@ -31,9 +70,9 @@ module.exports = yeoman.Base.extend({
         this.spawnCommandSync('docker', ['pull','store/citrix/netscalercpx:12.0-41.16']);
         this.log('\n');
         this.log(chalk.white('Starting and configuring the container.'));
-
+        
         //starting the netscaler CPX container
-        this.spawnCommandSync('docker',['run','-dt','-e','EULA=yes','-dt','-p','22','-p','80','-p','161/udp','--cap-add=NET_ADMIN','store/citrix/netscalercpx:11.53-11']);
+        this.spawnCommandSync('docker',['run','-e','EULA=yes','-dt','-p',this.config.get('nsCPXSSHPort') + ':22','-p',this.config.get('nsCPXHttpPort') + ':80','-p',this.config.get('nsCPXSNMPPort') + ':161/udp','--cap-add=NET_ADMIN','store/citrix/netscalercpx:12.0-41.16']);
     }
     //call the dotnet restore command.
     this.spawnCommand('dotnet',['restore']);
